855d3caca45ec253c3f12805022a643229f1f4f8
Ash <ashraf97@yandex.ru>
2022-08-22 16:47:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const https = require("https");
+var fs = require("fs");
 
 const homeStartingContent = "Привет! Меня зовут Ашраф. Я живу мечтой стать разработчиком. Хочу в ближайшем будущем работать WEB разработчиком и прикладываю на это свои силы. Этот вэб-блог показывает мое стремление им стать! Здесь будут представлены мои работы и проекты, которыми я могу гордиться!";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 const posts = [];
-const port = process.env.PORT;
+
 
 const app = express();
 
@@ -65,7 +67,11 @@ app.get("/posts/:postName", (req, res) => {
 
 })
 
+let PORT = process.env.PORT
+if (PORT == null || PORT == "") {
+  PORT = 3000;
+}
 
-app.listen(port, function() {
-  console.log(`Server started on port ${port}`);
+app.listen(PORT, function () {
+  console.log("Server started on port " + PORT);
 });
